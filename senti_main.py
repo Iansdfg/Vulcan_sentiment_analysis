e813a0da37202d33e3881ab0a5efa3c90de4f8be
Tian Feng <feng.txy@outlook.com>
2019-08-08 14:59:59 -0700

    Update senti_main.py

--- senti_main.py
+++ senti_main.py
@@ -1,35 +1,9 @@
 from sentiment_functions import get_senti
-
+from sentiment_functions import get_data
 
 if __name__ == '__main__':
-    documents = [
-        {
-            "id": "1",
-            "language": "en",
-            "text": "I had the best day of my life.",
-            "time": "12:34:32"
-        },
-        {
-            "id": "2",
-            "language": "en",
-            "text": "This was a waste of my time. The speaker put me to sleep.",
-            "time": "12:34:32"
-        },
-        {
-            "id": "3",
-            "language": "es",
-            "text": "No tengo dinero ni nada que dar...",
-            "time": "12:34:32"
-        },
-        {
-            "id": "4",
-            "language": "it",
-            "text": "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura.",
-            "time": "12:34:32"
-        }
-    ]
+    documents = get_data('test.json')
+    senti_res = get_senti(documents)
 
-    res = get_senti(documents)
-
-    for ele in res:
-        print(ele)
+    for ele in senti_res:
+        print(ele['Document Id'],ele['Sentiment Score'], ele['Time'],ele['Sentence'])
