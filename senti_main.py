ed4bf6ef96a48f070e6e0417662b0e4e3a779b67
Tian Feng <feng.txy@outlook.com>
2019-08-08 16:49:43 -0700

    Update senti_main.py

--- senti_main.py
+++ senti_main.py
@@ -1,19 +1,15 @@
 from sentiment_functions import get_senti
 from sentiment_functions import get_data
-<<<<<<< HEAD
 
 from CRUD_m import create_data
 from CRUD_m import read_data
 from CRUD_m import close_connection
 from CRUD_m import get_connection
 
-=======
->>>>>>> 28c84762a7ffa2836db0febf6ebfcfb6e76b7ac0
 
 if __name__ == '__main__':
     documents = get_data('test.json')
     senti_res = get_senti(documents)
-<<<<<<< HEAD
 
     connection = get_connection()
 
@@ -28,11 +24,6 @@ if __name__ == '__main__':
                 'Sentence':sigle_dic['Sentence']
                 }
 
-        print(data)
+        # print(data)
         table_name = 'text_analytics'
         create_data(table_name, data, connection)
-=======
-
-    for ele in senti_res:
-        print(ele['Document Id'],ele['Sentiment Score'], ele['Time'],ele['Sentence'])
->>>>>>> 28c84762a7ffa2836db0febf6ebfcfb6e76b7ac0
