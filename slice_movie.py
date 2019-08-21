30efc93049f796b3828b074f9ab2871422a46d24
Tian Feng <feng.txy@outlook.com>
2019-08-20 19:37:15 -0700

    Add files via upload

---
+++ slice_movie.py
@@ -0,0 +1,17 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+name = "Defuse_conflicts_62s"
+max = 119
+
+
+
+
+def slice_video(name, max):
+    for i in range(0, max, 10):
+        start = i
+        end = i + 10
+        ffmpeg_extract_subclip(F"/Users/Apple/Desktop/{name}.mp4", start, end, targetname= F"/Users/Apple/Desktop/Sliced_movie/{name}_{i}.mp4")
+
+
+if __name__ == '__main__':
+    slice_video(name, max)
