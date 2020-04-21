96ae3d574bb7b32b5e73a379784e5da7d5abc50b
Tian Feng <feng.txy@outlook.com>
2020-04-20 22:04:47 -0700

    Update senti_main.py

--- senti_main.py
+++ senti_main.py
@@ -6,7 +6,6 @@ from CRUD_m import read_data
 from CRUD_m import close_connection
 from CRUD_m import get_connection
 
-
 if __name__ == '__main__':
     documents = get_data('test.json')
     senti_res = get_senti(documents)
@@ -16,8 +15,6 @@ if __name__ == '__main__':
     for sigle_dic in senti_res:
         video_id = '001'
         #value = 0.7
-
-        
         data = {
                 'video_id': video_id,
                 'doc_id': sigle_dic['Document Id'],
